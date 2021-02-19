7797010ee127e7adf0a67fc4706373a367f69e2e
Achang8 <zzang8873@naver.com>
2021-02-19 12:56:16 +0900

    First Experiment

--- blocks.py
+++ blocks.py
@@ -1,11 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.nn.modules import conv
-from torch.nn.modules.batchnorm import BatchNorm2d
 
-def conv_bn(inp, oup, kernel_size, stride, 
+def conv_block(inp, oup, kernel_size, stride, upsampling = False, Leaky = False, InstanceNorm = False,
                 conv_layer = nn.Conv2d, norm_layer = nn.BatchNorm2d, act_layer = nn.ReLU):
+    if (upsampling):
+        conv_layer = nn.ConvTranspose2d
+    if (Leaky):
+        act_layer = nn.LeakyReLU(0.2)
+    if (InstanceNorm):
+        norm_layer = nn.InstanceNorm2d
+    
     return nn.Sequential(
         #bias는 computational cost 줄이기위해 false
         conv_layer(inp, oup, kernel_size, stride, 1, bias = False),
@@ -13,11 +18,9 @@ def conv_bn(inp, oup, kernel_size, stride,
         act_layer(inplace = True)       
     )
 
-def conv_1x1_bn(inp, oup, conv_layer = nn.Conv2d, norm_layer = nn.BatchNorm2d, act_layer = nn.ReLU):
+def conv_3x3(inp, oup, stride = 1, conv_layer = nn.Conv2d):
     return nn.Sequential(
-        conv_layer(inp, oup, 1, 1, 0, bias = False),
-        norm_layer(oup),
-        act_layer(inplace = True)
+        conv_layer(inp, oup, 3, stride, 1, bias = False),
     )
 
 class ResBlock(nn.Module):
@@ -37,3 +40,47 @@ class ResBlock(nn.Module):
         x = torch.add(x, residual)
         out = self.relu(x)
         return x
+    
+class SELayer(nn.Module):
+    def __init__(self, channels, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channels, channels // reduction, bias=False),
+            nn.LeakyReLU(0.2,inplace=True),
+            nn.Linear(channels // reduction, channels, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+    
+class SEBasicBlock(nn.Module):
+    def __init__(self, inp, oup, kernel_size, stride=1, reduction=16):
+        super(SEBasicBlock, self).__init__()
+        self.conv_block = conv_block(inp, oup, kernel_size, stride, Leaky = True, InstanceNorm = True)
+        self.conv = conv_3x3(inp, oup)
+        self.In = nn.InstanceNorm2d(oup)
+        self.se = SELayer(oup, reduction)
+        self.LReLU = nn.LeakyReLU(0.2)
+        self.downsample = downsample
+
+    def forward(self, x):
+        residual = x
+        out = self.conv_block(x)
+
+        out = self.conv(out)
+        out = self.In(out)
+        out = self.se(out)
+        '''
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        '''     
+        out = self.LRELU(out)
+
+        return out
